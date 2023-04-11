534d1597d1bd5f1cec0aa607d46415d2400aa926
FedeCarballo <carballo523@gmail.com>
2023-04-11 12:29:31 -0300

    Update app.js

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -17,7 +17,14 @@ app.use('/api/v1/tasks',tasks)
 // get :id <-- obtener una tarea por id
 // patch <-- Actualizar una tarea
 // delete <-- Eliminar una tarea
-
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+  });
+  
 //Con esta const lo que hacemos es crear una funcion asincrona, realizamos primero la conexion con la db y luego iniciamos el servidor, de no ser posible lo captura con el .catch
 //Utilizamos variable de entorno para pasar la url por .env
 const start = async () => {
