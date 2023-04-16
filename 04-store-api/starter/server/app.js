5958cce663ad8637802e366215bbb3ab01d23496
FedeCarballo <carballo523@gmail.com>
2023-04-16 15:40:08 -0300

    Update app.js

--- 04-store-api/starter/server/app.js
+++ 04-store-api/starter/server/app.js
@@ -1,24 +1,30 @@
-require('dotenv').config()
-require('express-async-errors')
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const express = require('express')
-const app = express();
+require('dotenv').config()
+require('express-async-errors')
 const port = process.env.PORT || 3000;
 const connectDB = require('./db/connect')
+
+
 const productsRouter = require('./routes/products')
 const cors = require('cors')
+const app = express();
 app.use(cors())
-app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(cookieParser());
+app.use(express.static(path.join(__dirname,'public')));
+app.use(express.json());
 app.use(morgan('dev'));
+app.use(cors({ origin: "*" }));
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+  );
   next();
 });
 // Middlewares
