d5b7e0da6128eff088d0fa1df51b9910497f847a
FedeCarballo <carballo523@gmail.com>
2023-04-16 15:35:24 -0300

    Update app.js

--- 04-store-api/starter/server/app.js
+++ 04-store-api/starter/server/app.js
@@ -1,12 +1,26 @@
 require('dotenv').config()
 require('express-async-errors')
-
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const express = require('express')
 const app = express();
 const port = process.env.PORT || 3000;
 const connectDB = require('./db/connect')
 const productsRouter = require('./routes/products')
-
+const cors = require('cors')
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(cookieParser());
+app.use(morgan('dev'));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 // Middlewares
 const NotFoundMiddleware = require('./middleware/not-found')
 const ErrorMiddleware = require('./middleware/error-handler')
