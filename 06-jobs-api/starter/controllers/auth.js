8ad208fda6dab15e34372447445febe967771fec
FedeCarballo <carballo523@gmail.com>
2023-04-22 20:33:36 -0300

    logica login & register

--- 06-jobs-api/starter/controllers/auth.js
+++ 06-jobs-api/starter/controllers/auth.js
@@ -1,12 +1,26 @@
+require('dotenv').config()
 const User = require('../models/User')
 const {StatusCodes} = require('http-status-codes')
+const {BadRequestError, UnauthenticatedError} = require('../errors')
 const register = async (req,res) => {
     const user = await User.create({...req.body})
-    res.status(StatusCodes.CREATED).json({user})
+    const token = user.CreateJWT()
+    res.status(StatusCodes.CREATED).json({user:{name: user.name}, token })
 }
-
 const login = async (req,res) => {
-    res.send('login user')
+    console.log(req.body);
+    const {email, password} = req.body
+
+    if(!email || !password){
+        throw new BadRequestError('Please provide email and password')
+    }
+    const user = await User.findOne({ email })
+    //Validacion de autenticacion, es decir que existe el usuario
+    if(!user){
+        throw new UnauthenticatedError('Invalid credentials')
+    }
+    const token = user.CreateJWT()
+    res.status(StatusCodes.OK).json({user:{name: user.name}, token})
 }
 
 module.exports = {
