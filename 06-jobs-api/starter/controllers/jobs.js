a4115bad198efc965656dc99a28cca6d6f501ab8
FedeCarballo <carballo523@gmail.com>
2023-04-26 15:46:21 -0300

    GetAll/Get/Create Jobs
    Jobs asociados a cada usuario en particular mediante token

--- 06-jobs-api/starter/controllers/jobs.js
+++ 06-jobs-api/starter/controllers/jobs.js
@@ -1,13 +1,30 @@
+const Job = require('../models/Job')
+const {StatusCodes} = require('http-status-codes')
+const {BadRequestError, NotFoundError} = require('../errors')
+
 const getAllJobs = async (req,res) => {
-    res.send('Get all jobs')
+    const jobs = await Job.find({createdBy:req.user.userId}).sort('createdAt')
+    res.status(StatusCodes.OK).json({jobs, count: jobs.length})
 }
 
 const GetJob = async (req,res) => {
-    res.send('Single Job')
+    const {user:{userId},params:{id:JobId}} = req
+    const job = await Job.findOne({
+        _id : JobId, 
+        createdBy:userId 
+    })
+    if(!job){
+        throw new NotFoundError(`Not job with ${JobId}`)
+    }
+    res.status(StatusCodes.OK).json({job})
 }
 
 const CreateJob = async (req,res) => {
-    res.json(req.user)
+   // Aca asociamos el job al usuario
+    req.body.createdBy = req.user.userId
+  
+    const job = await Job.create(req.body)
+    res.status(StatusCodes.CREATED).json({job})
 }
 
 const UpdateJob = async (req,res) => {
