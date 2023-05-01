6e64f7971b266f11c00912dd5f908b8c44ba4951
FedeCarballo <carballo523@gmail.com>
2023-05-01 16:00:56 -0300

    security cors

--- 06-jobs-api/starter/middleware/error-handler.js
+++ 06-jobs-api/starter/middleware/error-handler.js
@@ -10,6 +10,10 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     customError.msg = Object.values(err.errors).map((item => item.message)).join(',')
     customError.statusCode = 400
   }
+  if(err.name === 'CastError'){
+    customError.msg = `No item found with id ${err.value}`
+    customError.statusCode = 404
+  }
   if(err.code && err.code == 11000){
     customError.msg = ` ${Object.keys(err.keyValue)} duplicate, please choose another`
     customError.statusCode = 400
